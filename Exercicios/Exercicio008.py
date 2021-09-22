16b3b37f40fd9409b15df62a0ef2b0e4bdb5f515
FLINKBLINK <rvlucas1985@gmail.com>
2021-09-22 01:36:17 -0300

    mudei o exercicio 008

--- Exercicios/Exercicio008.py
+++ Exercicios/Exercicio008.py
@@ -1,4 +1,9 @@
 metro = int(input('Digite aqui o valor (em metros) que deseja converter: '))
-centimetro = metro * 100
+decimetro = metro * 10
+centimetro = decimetro * 10
 milimetro = centimetro * 10
-print('O valor em centimetros é de {} e em milimetros é de {}'.format(centimetro, milimetro)) 
+decametro = metro / 10
+hectometro = decametro / 10
+kilometro = hectometro / 10
+print('O valor em decímetros é de {}, em centimetros é de {} e em milimetros é de {}'.format(decimetro, centimetro, milimetro))
+print('O valor em decâmetro é de {}, em hectômetro é de {} e en kilômetro é de {}'.format(decametro, hectometro, kilometro)) 
