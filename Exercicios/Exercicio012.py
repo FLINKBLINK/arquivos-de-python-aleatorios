bdad55c9dfded568f48ff95516c5409608f4e60e
FLINKBLINK <rvlucas1985@gmail.com>
2021-09-22 02:11:40 -0300

    mudei o exercicio 012

--- Exercicios/Exercicio012.py
+++ Exercicios/Exercicio012.py
@@ -1,3 +1,3 @@
 preco = float(input('Digite o preço do produto: '))
-desc = (preco / 100) * 5 - preco
-print('O preço do produto com o desconto de 5%'+' ficou {}'.format(desc))
+desc = preco - (preco / 100) * 5
+print('O preço do produto com o desconto de 5%'+' ficou {:.2f}'.format(desc))
