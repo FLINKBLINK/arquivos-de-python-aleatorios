96877d510bfd764d1cb91ba6ddecfb0c6850002f
FLINKBLINK <rvlucas1985@gmail.com>
2021-09-22 01:23:04 -0300

    mudança

--- Exercicios/Exercicio007.py
+++ Exercicios/Exercicio007.py
@@ -2,4 +2,4 @@ nome = input('Digite o nome do(a) aluno(a) ao qual as notas vão ser atribuidas:
 nota1 = float(input('Digite a nota 01: '))
 nota2 = float(input('Digite a nota 02: '))
 media = (nota1 + nota2) / 2
-print('A média do(a) aluno(a) {} foi de {}'.format(nome, media))
+print('A média do(a) aluno(a) {} foi de {:.1f}'.format(nome, media))
